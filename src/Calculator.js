bb67a3dd9c00e231bc642d5bae44bf8da93d794f
Aidan-bn <bantezea@gmail.com>
2024-01-19 12:14:11 +0300

    Resolve stylelint errors

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,89 +1,155 @@
+import { useState } from 'react';
 import Nav from './Nav';
 
-const Calculator = () => (
-  <>
-    <Nav />
-    <section className="calc-section">
-      <h4>
-        Let us do some calculations!
-      </h4>
-      <div className="calculator">
-        <div className="top">
-          <input
-            type="text"
-            className="enter"
-            id="enter"
-          />
-          <input
-            type="text"
-            className="result"
-            id="enter"
-          />
+const Calculator = () => {
+  const [view, setView] = useState(1);
+  const change = () => {
+    setView(view + 10);
+  };
+
+  return (
+    <>
+      <Nav />
+      <section className="calc-section">
+        <h4>
+          Let us do some calculations!
+        </h4>
+        <div className="calculator">
+          <div className="top">
+            <input
+              type="text"
+              className="enter"
+              id="enter"
+            />
+            <input
+              type="text"
+              className="result"
+              id="result"
+            />
+          </div>
+          <div id="button" className="button">
+            <button
+              type="button"
+              className="btn-content"
+              onClick={change}
+            >
+              +/-
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              AC
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              c/o
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              +
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              7
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              8
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              9
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              x
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              4
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              5
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              6
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              -
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              1
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              2
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              3
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              /
+            </button>
+          </div>
+          <div id="lower-btn">
+            <button
+              type="button"
+              className="btn-content"
+            >
+              0
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              .
+            </button>
+            <button
+              type="button"
+              className="btn-content"
+            >
+              =
+            </button>
+          </div>
         </div>
-        <div id="button" className="button">
-          <div className="btn-content">
-            +/-
-          </div>
-          <div className="btn-content">
-            AC
-          </div>
-          <div className="btn-content">
-            c/o
-          </div>
-          <div className="btn-content">
-            +
-          </div>
-          <div className="btn-content">
-            7
-          </div>
-          <div className="btn-content">
-            8
-          </div>
-          <div className="btn-content">
-            9
-          </div>
-          <div className="btn-content">
-            x
-          </div>
-          <div className="btn-content">
-            4
-          </div>
-          <div className="btn-content">
-            5
-          </div>
-          <div className="btn-content">
-            6
-          </div>
-          <div className="btn-content">
-            -
-          </div>
-          <div className="btn-content">
-            1
-          </div>
-          <div className="btn-content">
-            2
-          </div>
-          <div className="btn-content">
-            3
-          </div>
-          <div className="btn-content">
-            /
-          </div>
-        </div>
-        <div id="lower-btn">
-          <div className="btn-content">
-            0
-          </div>
-          <div className="btn-content">
-            .
-          </div>
-          <div className="btn-content">
-            =
-          </div>
-        </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
+};
 
 export default Calculator;
